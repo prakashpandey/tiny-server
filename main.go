e12434b5f4f3de36525c8e93029d6696b745a2a6
Prakash Pandey <prakashpandeyy@yahoo.com>
2019-11-07 12:02:56 +0530

    Fixed parsing flags

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 
 func main() {
 	port := flag.Int("port", 8181, "Port at which the server will run.")
-	address := fmt.Sprintf(":%d", *port)
 	dir := flag.String("dir", ".", "Default directory from which file wil be served.")
+	flag.Parse()
+	address := fmt.Sprintf(":%d", *port)
 	fmt.Printf("Serving file from: %s directory.\n", *dir)
 	fmt.Printf("Running server at: %s\n", address)
 	if err := http.ListenAndServe(address, http.FileServer(http.Dir(*dir))); err != nil {
