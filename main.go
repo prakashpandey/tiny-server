85f66ae8ab6c714384819efa96c87d7e9915b899
Prakash Pandey <prakashpandeyy@yahoo.com>
2020-05-21 12:52:08 +0530

    Added functionality to download file if query param download = true

--- main.go
+++ main.go
@@ -4,16 +4,34 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func main() {
-	port := flag.Int("port", 8181, "Port at which the server will run.")
-	dir := flag.String("dir", ".", "Default directory from which file wil be served.")
+var (
+	dir  *string
+	port *int
+)
+
+func init() {
+	port = flag.Int("port", 8181, "Port at which the server will run.")
+	dir = flag.String("dir", ".", "Default directory from which file wil be served.")
 	flag.Parse()
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	download := strings.TrimSpace(r.URL.Query().Get("download"))
+	if download == "true" {
+		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", r.URL.Path))
+	}
+	http.FileServer(http.Dir(*dir)).ServeHTTP(w, r)
+}
+
+func main() {
+	// mustInitFlags()
 	address := fmt.Sprintf(":%d", *port)
 	fmt.Printf("Serving file from: %s directory.\n", *dir)
 	fmt.Printf("Running server at: %s\n", address)
-	if err := http.ListenAndServe(address, http.FileServer(http.Dir(*dir))); err != nil {
+	if err := http.ListenAndServe(address, http.HandlerFunc(rootHandler)); err != nil {
 		panic(err)
 	}
 }
